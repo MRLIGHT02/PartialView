24c506191b0961f7eccac580abd8b4640259b30b
MRLIGHT02 <saifali42020@gmail.com>
2025-07-18 15:19:29 +0530

    #

---
+++ PartialView/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Runtime.CompilerServices;
+
+namespace PartialView.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("/")]
+        [Route("home")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [Route("home/home-about")]
+        public IActionResult About()
+        {
+            return View();
+        }
+    }
+}
--- PartialView/Program.cs
+++ PartialView/Program.cs
@@ -1,6 +1,12 @@
 var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+app.UseStaticFiles();
+app.UseRouting();
+
+app.MapControllers();
+
 
 app.Run();
